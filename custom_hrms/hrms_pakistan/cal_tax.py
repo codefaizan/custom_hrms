e0ddaa9860c8e6e3caa3fbe6abb951aea10a71e2
shehroz.riaz <shehroz.riaz@inara.pk>
2025-06-27 21:34:45 +0500

    updated

--- custom_hrms/hrms_pakistan/cal_tax.py
+++ custom_hrms/hrms_pakistan/cal_tax.py
@@ -39,7 +39,9 @@ def custom_calculate_tax_by_tax_slab(annual_taxable_earning, tax_slab, eval_glob
             if flt(d.max_taxable_income) and flt(d.max_taxable_income) < annual_taxable_earning:
                 continue
             other_taxes_and_charges = tax_amount * flt(d.percent) / 100
-            tax_amount += other_taxes_and_charges
-            total_other_taxes_and_charges += other_taxes_and_charges
+            # tax_amount += other_taxes_and_charges
+            tax_amount = 0
+            total_other_taxes_and_charges = 0
+            # total_other_taxes_and_charges += other_taxes_and_charges
 
     return tax_amount, total_other_taxes_and_charges
