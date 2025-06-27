042c7a8c692204384914f09274c943b9248b5997
Faizan Ahmad <faizan.ahmad@inara.pk>
2025-06-27 17:03:17 +0500

    updated

--- custom_hrms/hrms_pakistan/cal_tax.py
+++ custom_hrms/hrms_pakistan/cal_tax.py
@@ -190,6 +190,97 @@ class SalarySlip(TransactionBase):
 		)
 
 		return current_tax_amount
+	
+	def get_working_days_details(self, lwp=None, for_preview=0):
+		payroll_settings = frappe.get_cached_value(
+			"Payroll Settings",
+			None,
+			(
+				"payroll_based_on",
+				"include_holidays_in_total_working_days",
+				"consider_marked_attendance_on_holidays",
+				"daily_wages_fraction_for_half_day",
+				"consider_unmarked_attendance_as",
+			),
+			as_dict=1,
+		)
+
+		consider_marked_attendance_on_holidays = (
+			payroll_settings.include_holidays_in_total_working_days
+			and payroll_settings.consider_marked_attendance_on_holidays
+		)
+
+		daily_wages_fraction_for_half_day = flt(payroll_settings.daily_wages_fraction_for_half_day) or 0.5
+
+		working_days = date_diff(self.end_date, self.start_date) + 1
+		if for_preview:
+			self.total_working_days = working_days
+			self.payment_days = working_days
+			return
+
+		holidays = self.get_holidays_for_employee(self.start_date, self.end_date)
+		working_days_list = [add_days(getdate(self.start_date), days=day) for day in range(0, working_days)]
+
+		if not cint(payroll_settings.include_holidays_in_total_working_days):
+			working_days_list = [i for i in working_days_list if i not in holidays]
+
+			working_days -= len(holidays)
+			if working_days < 0:
+				frappe.throw(_("There are more holidays than working days this month."))
+
+		if not payroll_settings.payroll_based_on:
+			frappe.throw(_("Please set Payroll based on in Payroll settings"))
+
+		if payroll_settings.payroll_based_on == "Attendance":
+			actual_lwp, absent = self.calculate_lwp_ppl_and_absent_days_based_on_attendance(
+				holidays, daily_wages_fraction_for_half_day, consider_marked_attendance_on_holidays
+			)
+			self.absent_days = absent
+		else:
+			actual_lwp = self.calculate_lwp_or_ppl_based_on_leave_application(
+				holidays, working_days_list, daily_wages_fraction_for_half_day
+			)
+
+		if not lwp:
+			lwp = actual_lwp
+		elif lwp != actual_lwp:
+			frappe.msgprint(
+				_("Leave Without Pay does not match with approved {} records").format(
+					payroll_settings.payroll_based_on
+				)
+			)
+
+		self.leave_without_pay = lwp
+		self.total_working_days = working_days
+
+		payment_days = self.get_payment_days(payroll_settings.include_holidays_in_total_working_days)
+
+		if flt(payment_days) > flt(lwp):
+			self.payment_days = flt(payment_days) - flt(lwp)
+
+			if payroll_settings.payroll_based_on == "Attendance":
+				self.payment_days -= flt(absent)
+
+			consider_unmarked_attendance_as = payroll_settings.consider_unmarked_attendance_as or "Present"
+
+			if (
+				payroll_settings.payroll_based_on == "Attendance"
+				and consider_unmarked_attendance_as == "Absent"
+			):
+				unmarked_days = self.get_unmarked_days(
+					payroll_settings.include_holidays_in_total_working_days, holidays
+				)
+				half_absent_days = self.get_half_absent_days(
+					payroll_settings.include_holidays_in_total_working_days,
+					consider_marked_attendance_on_holidays,
+					holidays,
+				)
+				self.absent_days += (
+					unmarked_days + half_absent_days * daily_wages_fraction_for_half_day
+				)  # will be treated as absent
+				self.payment_days -= unmarked_days + half_absent_days * daily_wages_fraction_for_half_day
+		else:
+			self.payment_days = 0
 
 def calculate_tax_by_tax_slab(annual_taxable_earning, tax_slab, eval_globals=None, eval_locals=None):
 	from hrms.hr.utils import calculate_tax_with_marginal_relief
